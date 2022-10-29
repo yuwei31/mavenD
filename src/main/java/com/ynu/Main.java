246b9711617f5a34dd44cae5e24a76769bca5ffc
yuwei31 <1075784400@qq.com>
2022-10-29 19:54:28 +0800

    Initial commit

---
+++ src/main/java/com/ynu/Main.java
@@ -0,0 +1,8 @@
+package com.ynu;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("This is a Github test!");
+    }
+}
