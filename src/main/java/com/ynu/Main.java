b0605f812f04250b2c68f3d7a567119cbf43a21e
yuwei31 <1075784400@qq.com>
2022-11-04 21:16:53 +0800

    加了呜呜呜呜呜

--- src/main/java/com/ynu/Main.java
+++ src/main/java/com/ynu/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("This is a Github test!");
         System.out.println("鼠鼠加了一行捏");
+        System.out.println("呜呜呜呜呜");
     }
 }
